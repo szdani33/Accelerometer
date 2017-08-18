2d87e5c727a2f44b10512ba77fce32f7d7c0607c
szdani33 <daniel.tamas.szabo@gmail.com>
2017-08-18 18:51:28 +0200

    Magnetometer added

---
+++ app/src/main/java/com/example/daniel_szabo/sensors/MagnetometerActivity.java
@@ -0,0 +1,13 @@
+package com.example.daniel_szabo.sensors;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MagnetometerActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_magnetometer2);
+    }
+}
